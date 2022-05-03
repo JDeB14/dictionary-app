3e9e6ef5a82592ff696772e6c91c9a321fbbec2d
JDeB14 <101413157+JDeB14@users.noreply.github.com>
2022-05-03 11:57:27 -0500

    Added search engine without API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  function handleSubmit(event) {
+  const [keyword, setKeyword] = useState(null);
+  function searchWord(event) {
     event.preventDefault();
+    alert(`Searching for ${keyword}...`);
+  }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
   }
 
   return (
     <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={searchWord}>
         <input
           type="search"
           placeholder="Search for a word.."
           autoFocus={true}
+          onChange={updateKeyword}
         />
         <input type="submit" value="Search" />
       </form>
