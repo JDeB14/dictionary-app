d232dfc85557b720760a5c393016d8089729c77d
JDeB14 <101413157+JDeB14@users.noreply.github.com>
2022-05-03 15:12:53 -0500

    Added mapping through definitions

--- src/Meanings.js
+++ src/Meanings.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export default function Meanings(props) {
   if (props.meaning) {
+    console.log(props.meaning.meanings);
     return (
       <div className="Meanings">
         <h2>{props.meaning.word}</h2>
         <h5>{props.meaning.phonetic}</h5>
-        {props.meaning.meanings.map(function (partOfSpeech, index) {
+        {props.meaning.meanings.map(function (array, index) {
           return (
             <div key={index}>
-              <h3>{partOfSpeech.partOfSpeech}</h3>
+              <h3>{array.partOfSpeech}</h3>
+              <p>{array.definitions[0].definition}</p>
             </div>
           );
         })}
