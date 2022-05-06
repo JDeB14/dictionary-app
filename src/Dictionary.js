5ee9aa658f6ee572ac8faa166772cc84bcec3b9c
JDeB14 <101413157+JDeB14@users.noreply.github.com>
2022-05-06 17:11:10 -0500

    Added Api integration for photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Meanings from "./Meanings";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -10,12 +11,22 @@ export default function Dictionary() {
   function showResponse(response) {
     setMeaning(response.data[0]);
   }
+
+  function showPhotos(response) {
+    console.log(response.data);
+  }
   function searchWord(event) {
     event.preventDefault();
 
     //https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(showResponse);
+
+    let pexelApiKey =
+      "563492ad6f91700001000001e59be451fbd64c43b10b8b22597744f6";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    const header = { Authorization: `Bearer ${pexelApiKey}` };
+    axios.get(pexelApiUrl, { headers: header }).then(showPhotos);
   }
 
   function updateKeyword(event) {
@@ -28,7 +39,7 @@ export default function Dictionary() {
         <form onSubmit={searchWord}>
           <input
             type="search"
-            placeholder="Search for a word..."
+            placeholder="Search for a word"
             autoFocus={true}
             onChange={updateKeyword}
           />
@@ -36,6 +47,7 @@ export default function Dictionary() {
         <div className="hint">i.e. wine, hug, plant, yoga</div>
       </section>
       <Meanings meaning={meaning} />
+      <Photos />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Photos() {
+  return <section className="Photos">Hello</section>;
+}
